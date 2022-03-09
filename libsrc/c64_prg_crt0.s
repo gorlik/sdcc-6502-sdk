e150c91f4095efe5fcfb443e6d814368d2996d86
gorlik <44734244+gorlik@users.noreply.github.com>
2022-03-09 13:06:17 -0800

    Use zero page for memcpy and memset parameters

--- libsrc/c64_prg_crt0.s
+++ libsrc/c64_prg_crt0.s
@@ -41,24 +41,24 @@ __sdcc_gs_init_startup:
 
 ; initialize DATA
         lda #<s_XINIT
-        sta ___memcpy_PARM_2
+        sta *___memcpy_PARM_2
         lda #>s_XINIT
-        sta ___memcpy_PARM_2+1
+        sta *___memcpy_PARM_2+1
         lda #<l_XINIT
-        sta ___memcpy_PARM_3
+        sta *___memcpy_PARM_3
         lda #>l_XINIT
-        sta ___memcpy_PARM_3+1
+        sta *___memcpy_PARM_3+1
         lda #<s_DATA
         ldx #>s_DATA
         jsr ___memcpy
 
 ; clear BSS
         lda #0x00
-        sta _memset_PARM_2
+        sta *_memset_PARM_2
         lda #<l_BSS
-        sta _memset_PARM_3
+        sta *_memset_PARM_3
         lda #>l_BSS
-        sta _memset_PARM_3+1
+        sta *_memset_PARM_3+1
         lda #<s_BSS
         ldx #>s_BSS
         jsr _memset
